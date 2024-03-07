c0d5630f4bf779932e33e9e83678dc1f911e549a
Guy Marshall <guymarshall9@gmail.com>
2024-03-07 15:21:19 +0000

    UPDATE: Move curly braces to new lines

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,33 @@
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         final int limit = 8;
 
-        for (int exponent = 3; exponent <= limit; exponent++) {
-            for (int x = 1; x <= limit; x++) {
+        for (int exponent = 3; exponent <= limit; exponent++)
+        {
+            for (int x = 1; x <= limit; x++)
+            {
                 boolean xIsEven = x % 2 == 0;
                 boolean xIsOdd = x % 2 != 0;
 
-                for (int y = 1; y <= limit; y++) {
+                for (int y = 1; y <= limit; y++)
+                {
                     boolean yIsEven = y % 2 == 0;
                     boolean yIsOdd = y % 2 != 0;
 
-                    for (int z = 1; z <= limit; z++) {
+                    for (int z = 1; z <= limit; z++)
+                    {
                         boolean zIsEven = z % 2 == 0;
                         boolean zIsOdd = z % 2 != 0;
 
-                        if (xIsEven && yIsOdd && zIsEven) {
+                        if (xIsEven && yIsOdd && zIsEven)
+                        {
                             continue;
                         }
 
-                        if (xIsOdd && yIsEven && zIsOdd) {
+                        if (xIsOdd && yIsEven && zIsOdd)
+                        {
                             continue;
                         }
 
@@ -27,7 +35,8 @@ public class Main {
                         final int yPowered = (int) Math.pow(y, exponent);
                         final int zPowered = (int) Math.pow(z, exponent);
 
-                        if (xPowered + yPowered == zPowered) {
+                        if (xPowered + yPowered == zPowered)
+                        {
                             System.out.printf("%d^%d + %d^%d = %d^%d%n", x, exponent, y, exponent, z, exponent);
                         }
                     }
